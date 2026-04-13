3b4420873b2fc97039954eb710178dff1aed2393
Texwondo Alex <alenbiruk13@gmail.com>
2026-04-12 21:47:43 -0700

    Refactor fetchData function for better HTML rendering
    Updated the fetchData function to correctly populate the HTML with fetched news articles and handle errors.

--- anbesa.js
+++ anbesa.js
@@ -6,11 +6,38 @@ async function fetchData(){
         const data = await response.json();
         let anbesahtml = "";
         document.getElementById("Totalr").innerHTML = data.totalResults;
-        document.getElementById("info").innerHTML = data.results.forEach(article => {
-        anbesahtml += `<div class="video"><p><strong><em> ID : ${article.source_priority * 35 + 6}<em><strong></div><div class="newspicture">${article.image_url}</div><div class="source"><img src="${article.source_icon}" alt="${article.source_name + "Icon"} class="sicon"><p class="sname">${article.source_name}</p></div><div class="creator">Creator : ${article.creator}</div><div class="title"><h2><a href="${article.link}">${article.title}</a></h2></div><div class="description">${article.description}</div><div class="time"><p class="pub">Published At : ${article.pubDate}</p><p class="fetch"> Fetched At : ${article.fetched_at}></div></div>`
-        }); ;
+        data.results.forEach(article => {
+anbesahtml += `<div class="video">
+<p><strong><em>ID : ${article.source_priority * 35 + 6}</em></strong></p>
+
+<div class="newspicture">
+<img src="${article.image_url}" alt="news image">
+</div>
+
+<div class="source">
+<img src="${article.source_icon}" alt="${article.source_name} Icon" class="sicon">
+<p class="sname">${article.source_name}</p>
+</div>
+
+<div class="creator">Creator : ${article.creator}</div>
+
+<div class="title">
+<h2><a href="${article.link}">${article.title}</a></h2>
+</div>
+
+<div class="description">${article.description}</div>
+
+<div class="time">
+<p class="pub">Published At : ${article.pubDate}</p>
+<p class="fetch">Fetched At : ${article.fetched_at}</p>
+</div>
+
+</div>`;
+});
+
+document.getElementById("info").innerHTML = anbesahtml;
     }
     catch{
-        document.getElementById("info").innerHTML = "<center><h1 class='scale-up-bottom error'><strong>ERROR 404:</strong><h1><h1 class='scale-up-bottom errorwhy'><strong><em>Error, failed to load resource please try again!</em></strong><h1><p class='sscale-up-bottom'><em>Maybe be no internet connection or server error.</em></p></center>" ;
+        document.getElementById("info").innerHTML = "<center><p class='scale-up-bottom error'><strong>ERROR 404:</strong></p><p class='scale-up-bottom errorwhy'><strong><em>Error, failed to load resource please try again!</em></strong><p><p class='sscale-up-bottom'><em>Maybe be no internet connection or server error.</em></p></center>" ;
     }
 }
